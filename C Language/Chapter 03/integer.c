cf4df820e10aca5a77d0a41f88e18396c6080a4a
Leapense <Leapense@sillain.ac.kr>
2021-04-23 19:29:36 +0900

    Create Chapter 03/integer.c File.

---
+++ C Language/Chapter 03/integer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	char a = 10;
+	short b = 200;
+	int c = -30;
+	long d = -4000;
+	long long e = 70000;
+
+	printf("%d %d %d %ld %lld\n", a, b, c, d, e);
+
+	return 0;
+}
