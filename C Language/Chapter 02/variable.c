871f19a58b1a18d71f2c4297eb63fb81cfda8fad
Leapense <Leapense@sillain.ac.kr>
2021-04-23 18:50:10 +0900

    Create variable.c File.

---
+++ C Language/Chapter 02/variable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+	int num3;
+
+	num1 = 10;
+	num2 = 20;
+	num3 = 30;
+
+	printf("%d %d %d\n", num1, num2, num3);
+
+	return 0;
+}
+
+/*
+변수는 컴퓨터가 명령을 처리하는 도중
+발생하는 값을 저장하기 위한 공간이다.
+*/
