68f8485fc58cc5d754e3e5a086d4e01f76becd59
Leapense <Leapense@sillain.ac.kr>
2021-04-23 19:06:09 +0900

    Update variable.c

--- C Language/Chapter 02/variable.c
+++ C Language/Chapter 02/variable.c
@@ -7,11 +7,13 @@ int main()
 //	int num3;
 
 	// 더 간단하게 하는 방법이 있습니다.
-	int num1, num2, num3;
+//	int num1, num2, num3;
 
-	num1 = 10;
-	num2 = 20;
-	num3 = 30;
+//	num1 = 10;
+//	num2 = 20;
+//	num3 = 30;
+	// 변수이름 = 값으로 할 수 있습니다.
+	int num1 = 10, num2 = 20, num3 = 30;
 
 	printf("%d %d %d\n", num1, num2, num3);
 
