b3cf8cbbb883531f5e30dcea662848dbc832bdd3
Leapense <Leapense@sillain.ac.kr>
2021-04-24 19:17:22 +0900

    Create via files

---
+++ C Language/Chapter 07/scanf_char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	char c1;
+
+	scanf("%c", &c1);
+	printf("%c\n", c1);
+
+	return 0;
+}
---
+++ C Language/Chapter 07/scanf_real_number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	float num1;
+
+	scanf("%f", &num1);
+
+	printf("%f\n", num1);
+
+	return 0;
+}
